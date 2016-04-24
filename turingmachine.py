6491b9ee40f2745748ac6481e0f6e52ede522527
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2016-04-24 10:43:12 +0200

    object oriented

--- turingmachine.py
+++ turingmachine.py
@@ -1,22 +1,31 @@
 import json
 
-json_data = open('instructions.json').read()
 
-instructions = json.loads(json_data)
+class TuringMachine(object):
+    def __init__(self, instructions, tape, end_state, current_state):
+        self.instructions = instructions
+        self.tape = tape
+        self.end_state = end_state
+        self.current_state = current_state
+
+    def cycle(self):
+        i = 0
+        while self.current_state != self.end_state:
+            if i < len(self.tape):
+                current_cell = self.tape[i]
+            else:
+                current_cell = "B"
+                self.tape.append("B")
+            self.tape[i] = instructions[self.current_state][current_cell]["write"]
+            i += instructions[self.current_state][current_cell]["move"]
+            self.current_state = instructions[self.current_state][current_cell]["nextState"]
+        return self.tape
 
 
-def turing_machine(instructions, tape, end_state, current_state):
-    i = 0
-    while current_state != end_state:
-        if i < len(tape):
-            current_cell = tape[i]
-        else:
-            current_cell = "B"
-            tape.append("B")
-        tape[i] = instructions[current_state][current_cell]["write"]
-        i += instructions[current_state][current_cell]["move"]
-        current_state = instructions[current_state][current_cell]["nextState"]
-    return tape
+if __name__ == '__main__':
+    try:
+        instructions = json.loads(open('instructions.json').read())
+        print(TuringMachine(instructions, list("111"), "q5", "q0").cycle())
+    except Exception as e:
+        print("Looks like the .json-File is invalid!")
 
-
-print(turing_machine(instructions, list("111"), "q5", "q0"))
