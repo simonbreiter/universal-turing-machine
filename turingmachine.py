7a7bbaf19882a1d6f2fe8e2ea666605710409ba0
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2017-10-07 23:37:28 +0200

    Make turingmachine file pep8 conform

--- turingmachine.py
+++ turingmachine.py
@@ -9,15 +9,23 @@ import os
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Implementation of a universal Turing Machine.')
-    parser.add_argument('-b', '--initial', type=str, action='store', default='q0', help='Initial state to begin')
-    parser.add_argument('-e', '--end', type=str, action='store', default='qdone', help='End state')
-    parser.add_argument('-s', '--speed', type=float, action='store', default=.3, help='Rendering speed in seconds')
-    parser.add_argument('-r', '--render', action='store_true', default=False, help='Render turing machine')
-    parser.add_argument('-a', '--interactive', action='store_true', default=False, help='Interactive mode, speed will be useless when using this parameter')
+    parser = argparse.ArgumentParser(
+        description='Implementation of a universal Turing Machine.')
+    parser.add_argument('-b', '--initial', type=str, action='store',
+                        default='q0', help='Initial state to begin')
+    parser.add_argument('-e', '--end', type=str,
+                        action='store', default='qdone', help='End state')
+    parser.add_argument('-s', '--speed', type=float, action='store',
+                        default=.3, help='Rendering speed in seconds')
+    parser.add_argument('-r', '--render', action='store_true',
+                        default=False, help='Render turing machine')
+    parser.add_argument('-a', '--interactive', action='store_true', default=False,
+                        help='Interactive mode, speed will be useless when using this parameter')
     required_arguments = parser.add_argument_group('required arguments')
-    required_arguments.add_argument('-i', '--instructions', type=str, action='store', default=False, required=True, help='Instructions, as JSON file')
-    required_arguments.add_argument('-t', '--input', type=str, action='store', default=False, required=True, help='Input tape')
+    required_arguments.add_argument('-i', '--instructions', type=str, action='store',
+                                    default=False, required=True, help='Instructions, as JSON file')
+    required_arguments.add_argument(
+        '-t', '--input', type=str, action='store', default=False, required=True, help='Input tape')
     return parser.parse_args()
 
 
@@ -48,7 +56,8 @@ class TuringMachine(object):
                 cell = self.empty_sign
                 self.tape.append(self.empty_sign)
             self.tape[index] = self.instructions[self.state][cell]['write']
-            index += self.get_direction(self.instructions[self.state][cell]['move'])
+            index += self.get_direction(
+                self.instructions[self.state][cell]['move'])
             self.state = self.instructions[self.state][cell]['nextState']
             if self.render:
                 self.render_screen(index, counter)
@@ -60,14 +69,16 @@ class TuringMachine(object):
         padding_start = visible_tape - index
         padding_end = visible_tape - (len(self.tape) - (index + 1))
         dynamic_start = index - visible_tape if index >= visible_tape else 0
-        dynamic_end = len(self.tape) - (len(self.tape) - index - visible_tape) if len(self.tape) - index > visible_tape else len(self.tape)
+        dynamic_end = len(self.tape) - (len(self.tape) - index - visible_tape) if len(
+            self.tape) - index > visible_tape else len(self.tape)
         os.system('clear')
         print('Steps Counter {}'.format(str(counter).rjust(7)))
         print('Current State {}'.format(self.state.rjust(7)))
         print('Tape Index {} '.format(str(index).rjust(10)))
-        print(visible_tape*2 * ' ' + '▼')
-        self.print_with_pipes(padding_start * ' ' + self.tape_as_string()[dynamic_start:dynamic_end] + padding_end * ' ')
-        print(visible_tape*2 * ' ' + '▲')
+        print(visible_tape * 2 * ' ' + '▼')
+        self.print_with_pipes(padding_start * ' ' + self.tape_as_string()
+                              [dynamic_start:dynamic_end] + padding_end * ' ')
+        print(visible_tape * 2 * ' ' + '▲')
         self.print_sign_occurences()
         if self.interactive:
             input()
@@ -87,21 +98,23 @@ class TuringMachine(object):
             print('{}x: {}'.format(v, k))
         print('\n')
 
-    @staticmethod
-    def print_with_pipes(string):
-        print('|'.join(string[i:i+1] for i in range(0, len(string))))
-
     def validate_instruction(self):
         for instruction in self.instructions:
             for case in self.instructions[instruction]:
                 state_to_check = self.instructions[instruction][case]['nextState']
                 if state_to_check not in self.instructions and state_to_check != self.end_state:
-                    raise Exception('Invalid configuration, the state {} does not exist!'.format(state_to_check))
+                    raise Exception(
+                        'Invalid configuration, the state {} does not exist!'.format(state_to_check))
 
     def tape_as_string(self):
         return str.join('', self.tape)
 
-    def get_direction(self, direction):
+    @staticmethod
+    def print_with_pipes(string):
+        print('|'.join(string[i:i + 1] for i in range(0, len(string))))
+
+    @staticmethod
+    def get_direction(direction):
         directions = {'right': 1, 'left': -1}
         return directions[direction] if direction in directions else 0
 
@@ -110,7 +123,13 @@ def main():
     args = parse_arguments()
     try:
         instructions = json.loads(open(args.instructions).read())
-        print('Input: {} \nOutput: {}'.format(args.input, TuringMachine(instructions, args.input, args.initial, args.end, args.render, args.speed, args.interactive).run()))
+        print('Input: {} \nOutput: {}'.format(args.input, TuringMachine(instructions,
+                                                                        args.input,
+                                                                        args.initial,
+                                                                        args.end,
+                                                                        args.render,
+                                                                        args.speed,
+                                                                        args.interactive).run()))
     except Exception as e:
         print('Something went wrong! Issue: {}'.format(e))
 
